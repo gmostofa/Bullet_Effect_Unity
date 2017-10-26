574747d952957d451ab609346e52c15d8aad3f3d
G_Mostofa <mostofart@gmail.com>
2017-10-26 18:58:39 +0600

    An example project in unity about bullet time effect.

---
+++ Assets/_scripts/BulletTimeEffectScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTimeEffectScript : MonoBehaviour {
+
+    private float slowMoTimeScale;  //slow motion time scale
+    private float fastMoTimeScale;  //fast motion time scale
+    private float factor = 4f;      //factor to increase or decrease the timescale by
+    // Use this for initialization
+    void Start()
+    {
+        slowMoTimeScale = Time.timeScale / factor;    //calculate the new timescale
+        fastMoTimeScale = Time.timeScale * factor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate the cube at a constant rate
+        transform.Rotate(new Vector3(200, 200, 200) * Time.deltaTime);
+    }
+
+    void OnGUI()
+    {
+        //Pause/Unpause on click
+        if (GUI.Button(new Rect(0, 0, 120, 30), "PAUSE/RESUME"))
+        {
+            CheckPaused();
+        }
+        //add slow motion effect
+        if (GUI.Button(new Rect(0, 30, 80, 30), "SLOWMO"))
+        {
+            slowMo();
+        }
+        //add fast motion effect
+        if (GUI.Button(new Rect(0, 60, 120, 30), "FASTMO"))
+        {
+            fastMo();
+        }
+    }
+
+    //check if paused
+    void CheckPaused()
+    {
+        if (Time.timeScale > 0)
+        {
+            pauseGame();
+        }
+        else
+        {
+            unPauseGame();
+        }
+    }
+    //pause the game
+    void pauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    //unpause the game
+    void unPauseGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    //add slow motion effect
+    void slowMo()
+    {
+        //assign new time scale value
+        Time.timeScale = slowMoTimeScale;
+        //reduce this to the same proportion as timescale to ensure smooth simulation
+        Time.fixedDeltaTime = Time.fixedDeltaTime * Time.timeScale;
+    }
+
+    //add fast motion effect
+    void fastMo()
+    {
+        Time.timeScale = fastMoTimeScale;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * Time.timeScale;
+    }
+}
---
+++ Assets/_scripts/PlayerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    //factor for cube movement speed
+    float speed = 10f;
+    void Update()
+    {
+        //get input from keyboard i.e. the arrow keys
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        //add the input values to the Player so as to move it
+        transform.position += new Vector3(h, 0, v) * Time.deltaTime * speed * 1 / Time.timeScale;
+    }
+}
